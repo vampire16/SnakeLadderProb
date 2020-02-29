721d9dfc241232e0c3cb412b40e8518e991a7ac6
vampire16 <siddheshsalvi525@gmai.com>
2020-02-29 15:33:25 +0530

    MODIFIED : Removed unwanted spaces

--- snakeladder/SnakeLadder.sh
+++ snakeladder/SnakeLadder.sh
@@ -20,7 +20,7 @@ index=1
 declare -A DiceDictionary
 
 #CHECKING WHICH PLAYER WON THE GAME
-while (( position1 < 100 && position2 < 100))
+while (( position1 < 100 && position2 < 100 ))
 do
 	for (( playerIndex=1; playerIndex<=PLAYER2; playerIndex++ ))
 	do
@@ -44,21 +44,21 @@ do
 			esac
 		else
 			Die=$((RANDOM%6 + 1))
-         count2=$(($count2+1))
-         randomCheck=$((RANDOM%3))
-         case $randomCheck in
-         $NOPLAY)
-            position2=$(($position2+$NOPLAY))
-            ;;
-         $SNAKE)
-            position2=$(($position2-$Die))
-            if (( position2 < 0 )); then position2=0; fi
-            ;;
-         $LADDER)
-            position2=$(($position2+$Die))
-            if (( position2 > 100 )); then position2=$(($position2-$Die)); fi
-            ;;
-         esac
+			count2=$(($count2+1))
+			randomCheck=$((RANDOM%3))
+			case $randomCheck in
+			$NOPLAY)
+				position2=$(($position2+$NOPLAY))
+				;;
+			$SNAKE)
+				position2=$(($position2-$Die))
+				if (( position2 < 0 )); then position2=0; fi
+				;;
+			$LADDER)
+			position2=$(($position2+$Die))
+				if (( position2 > 100 )); then position2=$(($position2-$Die)); fi
+				;;
+			esac
 		fi
 	done
 done
