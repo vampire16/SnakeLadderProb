44e7c8d622ad860fb3f7b1920ffa5328f4c4bbbe
vampire16 <siddheshsalvi525@gmai.com>
2020-02-29 12:30:19 +0530

    ADDED : options for snake,ladder and noPlay

--- snakeladder/SnakeLadder.sh
+++ snakeladder/SnakeLadder.sh
@@ -3,6 +3,9 @@
 echo "Welcome"
 #CONSTANT 
 PLAYER=1
+NOPLAY=0
+SNAKE=1
+LADDER=2
 
 #VARIABLES
 position=0
@@ -10,3 +13,17 @@ position=0
 #RANDOM FUNCTION
 Die=$((RANDOM%6 + 1))
 echo "$Die"
+
+#CHECKING FOR OPTIONS
+randomCheck=$((RANDOM%3))
+case $randomCheck in
+$NOPLAY)
+	NOPLAY=$(($position+$NOPLAY))
+	;;
+$SNAKE)
+	SNAKE=$(($position-$Die))
+	;;
+$LADDER)
+	LADDER=$(($position+$Die))
+	;;
+esac
