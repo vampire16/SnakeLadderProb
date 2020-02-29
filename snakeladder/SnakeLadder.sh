63e3f4c47997022de59daae2808f2c254b5f3a90
vampire16 <siddheshsalvi525@gmai.com>
2020-02-29 14:54:18 +0530

    ADDED : Printing number of dice need to win and positions

--- snakeladder/SnakeLadder.sh
+++ snakeladder/SnakeLadder.sh
@@ -10,11 +10,16 @@ LADDER=2
 
 #VARIABLES
 position=0
+count=0
+index=1
+
+declare -A DiceDictionary
 
 #CHECKING FOR OPTIONS AND REPEATING DIE TILL PLAYER REACHES THE WINNING POSITION
 while (( position < 100 ))
 do
 	Die=$((RANDOM%6 + 1))
+	count=$(($count+1))
 	randomCheck=$((RANDOM%3))
 	case $randomCheck in
 	$NOPLAY)
@@ -29,4 +34,12 @@ do
 		if (( position > 100 )); then position=$(($position-$Die)); fi
 		;;
 	esac
+	DiceDictionary[$count]=$position
+done
+
+#PRINTING NUMBER OF DICE AND POSITION AFTER EVERY DICE ROLE
+while (( $index <= $count ))
+do
+	echo "$index---->${DiceDictionary[$index]}"
+	index=$(($index+1))
 done
